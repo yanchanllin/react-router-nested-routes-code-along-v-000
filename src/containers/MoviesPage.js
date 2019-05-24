da168d2996e1c9c54e32b84afd28389a799795d7
Yan C Lin <yonalin1@gmail.com>
2019-05-24 18:54:39 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -10,7 +10,7 @@ const MoviesPage = ({ match, movies }) => (
     <Route exact path={match.url} render={()=> (
       <h3>Please select a movie from the list.</h3>
     )} />
-    <Route path={'${match.url}/:movieId'} render={routerProps => <MovieShow movies={movies} 
+    <Route path={'${match.url}/:movieId'} render={routerProps => <MovieShow movies={movies}
     {...routerProps} /> } />
   </div>;
 
